bdc6cda2ecda42a932588d97d3a24a219ec96227
Raymond Brown <daimyoplays@gmail.com>
2022-07-13 21:42:55 -0500

    redux, router, and stuff

--- umbrello/src/App.js
+++ umbrello/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Hi!</h1>
+      <Navbar />
     </div>
   );
 }
